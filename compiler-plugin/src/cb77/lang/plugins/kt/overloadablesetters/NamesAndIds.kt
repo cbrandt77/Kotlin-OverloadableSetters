e533a627e24711eb51fbf7731739ac1a5f489445
cbrandt77 <84159639+cbrandt77@users.noreply.github.com>
2025-11-30 17:36:28 -0500

    NamesAndIds: remove unused stuff

--- compiler-plugin/src/cb77/lang/plugins/kt/overloadablesetters/NamesAndIds.kt
+++ compiler-plugin/src/cb77/lang/plugins/kt/overloadablesetters/NamesAndIds.kt
@@ -1,14 +1,11 @@
 package cb77.lang.plugins.kt.overloadablesetters
 
+import org.jetbrains.kotlin.builtins.StandardNames
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name
 
 object NamesAndIds {
-	val ANNOT_JVMNAME = FqName("kotlin.jvm.JvmName")
+	
 	val ANNOT_HASCUSTOMSETTERS = FqName("cb77.lang.plugins.kt.overloadablesetters.HasCustomSetters")
 	val ANNOT_DEFAULTSETTERMETADATA = FqName("cb77.lang.plugins.kt.overloadablesetters.OverloadableSettersDefaultSetterMetadata")
-	
-	val NAME = Name.identifier("name")
-	val TYPE = Name.identifier("type")
-	val DEFAULT_SETTER_PARAM_NAME = Name.identifier("value")
 }
